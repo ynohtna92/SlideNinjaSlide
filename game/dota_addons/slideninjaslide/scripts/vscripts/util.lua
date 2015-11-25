6c4995524615451466311485981354cac1535c21
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2015-11-25 20:57:57 +1100

    Added notifications lib

--- game/dota_addons/slideninjaslide/scripts/vscripts/util.lua
+++ game/dota_addons/slideninjaslide/scripts/vscripts/util.lua
@@ -6,6 +6,12 @@ function GiveUnitDataDrivenModifier(source, target, modifier,dur)
     item:RemoveSelf()
 end
 
+function SendErrorMessage( pID, string )
+	Notifications:ClearBottom(pID)
+	Notifications:Bottom(pID, {text=string, style={color='#E62020'}, duration=2})
+	EmitSoundOnClient("General.Cancel", PlayerResource:GetPlayer(pID))
+end
+
 function HasFullInventory( unit )
 	local count = 0
 	for i=0,5 do
